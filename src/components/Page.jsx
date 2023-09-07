479ca03f5fd07ec6c8996115bc842a8c65794e76
Lucas Luna <lucaslunarj@gmail.com>
2023-09-07 20:04:21 -0300

    remove function added

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -8,14 +8,16 @@ import { useContext } from "react"
 export const Page = () => {
     const context = useContext(DataContext)
     console.log(context.state.posts.post)
-
+    function handleRemove(id) {
+        console.log(id)
+    }
     return (
         <div className="bg-black w-full h-full">
             <>
 
                 <div className="flex flex-col items-center justify-center gap-2 ">
                     <Header />
-                    {context.state.posts.map((item) => <Post key={item.posts.id} title={item.posts.title} post={item.posts.post} />)}
+                    {context.state.posts.map((item) => <Post key={item.posts.id} title={item.posts.title} post={item.posts.post} removePost={() => handleRemove(item.posts.id)} />)}
                 </div>
 
             </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,12 @@
 export const Post = (props) => {
-    const [title, post] = props
+
     return (
         <div className="w-screen">
             <div className="border border-solid border-blue-700  h-auto w-1/2 m-auto">
                 <p className="text-white">{props.title}</p>
                 <p className="text-white m-3">{props.post}</p>
                 <div>
-                    <button className="text-white rounded-md p-1 m-2 hover:border-red-700 border border-solid border-red-500">Excluir</button>
+                    <button onClick={props.removePost} className="text-white rounded-md p-1 m-2 hover:border-red-700 border border-solid border-red-500">Excluir</button>
                     <button className=" text-white border border-solid border-yellow-500 m-2 p-1 rounded-md " >Editar</button>
                 </div>
             </div>
